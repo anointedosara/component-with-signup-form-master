8a3415b894735e2dd800dd459bc019a739f17ba1
anointedosara <101458383+anointedosara@users.noreply.github.com>
2022-06-28 22:19:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let div6 = document.getElementsByClassName("div6")
+let div4 = document.querySelector(".div4")
+for (let i = 0; i < div6.length; i++) {
+    div6[i].addEventListener("click", () => {
+        div6[i].classList.add("border")
+
+        for (let j = 0; j < div6.length; j++) {
+            if (i != j) {
+                div6[j].classList.remove("border")
+            }
+        }
+    })
+}
+
+let input = document.getElementsByClassName("input")
+let form = document.getElementById("form")
+let img = document.getElementsByClassName("img")
+let div7 = document.getElementsByClassName("div7")
+
+for (let i = 0; i < input.length; i++) {
+    form.addEventListener("submit", (e) => {
+
+        if (input[i].value == "") {
+            e.preventDefault()
+            div6[i].classList.add("bord")
+            div6[i].style.marginBottom = 0
+            img[i].style.display = "block"
+            div7[i].style.display = "block"
+            div7[0].innerHTML = "Firstname cannot be empty"
+            div7[1].innerHTML = "Lastname cannot be empty"
+            div7[2].innerHTML = "Looks like this is not an email"
+            div7[3].innerHTML = "Password cannot be empty"
+        }
+
+        if (input[i].value != "") {
+            div6[i].classList.remove("bord")
+            div6[i].style.marginBottom = "30px"
+            img[i].style.display = "none"
+            div7[i].style.display = "none"
+        }
+
+        function validateEmail(emailId) {
+            result = emailId.value.includes("@gmail.com") || emailId.value.includes("@yahoo.com") || emailId.value.includes("@outlook.com")
+            if (result) {
+                div6[2].classList.remove("bord")
+                div6[2].style.marginBottom = "30px"
+                img[2].style.display = "none"
+                div7[2].style.display = "none"
+            } else {
+                div6[i].classList.add("bord")
+                div6[i].style.marginBottom = 0
+                div7[2].innerHTML = "Looks like this is not an email"
+                div7[2].style.display = "block"
+                img[i].style.display = "block"
+            }
+        }
+
+        let emailId = document.getElementById("input")
+        validateEmail(emailId)
+    })
+}
